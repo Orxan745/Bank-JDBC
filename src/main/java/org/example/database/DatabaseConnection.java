74549278e41c8de4c0f4da1e7c8f305c190c68bc
Orxan745 <orxan8586orxan@gmail.com>
2025-03-14 13:39:01 +0400

    commit

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    public static final String url = "jdbc:postgresql://localhost:5432/bank_customer";
-    public static final String username = "postgres";
-    public static final String password = "3459095";
+    private static final String url = "jdbc:postgresql://localhost:5432/bank_customer";
+    private static final String username = "postgres";
+    private static final String password = "3459095";
 
     public static Connection getDatabaseConnection(){
         try {
